97c9f5227cc2996c60a3be44cb70400b23dca013
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2019-04-23 17:47:32 +0200

    - prepare autocomplete component

---
+++ components/autocomplete.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    <ul>
+      <li v-for="(suggestion,i) in suggestions" :key="`suggestion_${i}`">
+        {{ suggestion }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    suggestions: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+<style>
+</style>
